98d8c51c26c8834a7ee41affc7588c67440856d2
GabrielSegatto <gabriel.segatto58@gmail.com>
2019-03-01 10:20:53 -0300

    add swipe

---
+++ final/script.js
@@ -0,0 +1,167 @@
+window.onload = function iniciar() {
+
+    // difinir qual elemento html contem a tela
+    var tela = document.getElementById('tela')
+
+    // ctx = contexto
+    var ctx = tela.getContext("2d")
+
+    //click do botão para mobile
+    document.getElementById('e').addEventListener('click', e)
+    document.getElementById('c').addEventListener('click', c)
+    document.getElementById('d').addEventListener('click', d)
+    document.getElementById('b').addEventListener('click', b)
+
+    //buscar pelo presionar da tecla
+    document.addEventListener("keydown", keyPush)
+
+    // executa a função a cada 80 mms
+    if (screen.width < 400) {
+        setInterval(jogo, 200)
+    } else {
+
+        setInterval(jogo, 80)
+    }
+
+    //velocidade do jogo
+    const vel = 1
+
+    //vx = velocidade horizontal , vy = velocidade vertical
+    var vx = vy = 0
+
+    // tamanho do pixel
+    var tp = 15
+
+    // quantidade de pessas
+    var qp = 20
+
+    //px e py = ponto de inico do jogo
+    var px = py = Math.floor(Math.random() * qp)
+
+    // possição da maça
+    var ax = ay = Math.floor(Math.random() * qp)
+
+    //tamanho da cobra
+    var trilha = [];
+    corpo = 5
+
+    //localizar a tag h2 pontuação
+    const p = document.getElementById('pontuacao')
+
+    function jogo() {
+        px += vx
+        py += vy
+
+        //mostrar a cobra do lado oposto
+        if (px < 0) {
+            px = qp - 1
+        } if (px > qp - 1) {
+            px = 0
+        } if (py < 0) {
+            py = qp - 1
+        } if (py > qp - 1) {
+            py = 0
+        }
+
+        //pintar o plano de fundo
+        ctx.fillStyle = "black"
+        ctx.fillRect(0, 0, tela.width, tela.height)
+
+        //pintar a maça
+        ctx.fillStyle = "red"
+        ctx.fillRect(ax * tp, ay * tp, tp, tp)
+
+        //pintar a cobra
+        ctx.fillStyle = "green"
+        for (var i = 0; i < trilha.length; i++) {
+            ctx.fillRect(trilha[i].x * tp, trilha[i].y * tp, tp, tp)
+
+            //se o corpo se toca reinicia o jogo
+            if (trilha[i].x == px && trilha[i].y == py) {
+                corpo = 5
+                trilha = []
+                vx = vy = 0
+            }
+        }
+
+        trilha.push({ x: px, y: py })
+        while (trilha.length > corpo) {
+            trilha.shift();
+        }
+
+        if (ax == px && ay == py) {
+
+            p.innerText = `Pontuação: ${corpo - 4}`
+            corpo++
+            ax = Math.floor(Math.random() * qp)
+            ay = Math.floor(Math.random() * qp)
+        }
+    }
+
+    function keyPush(event) {
+        switch (event.keyCode) {
+            case 37: // esquerda
+                if (vx != vel) {
+                    vx = -vel;
+                    vy = 0;
+                    break;
+                }
+            case 38: // cima
+                if (vy != vel) {
+                    vx = 0;
+                    vy = -vel;
+                    break;
+                }
+            case 39: // direita
+                if (vx != -vel) {
+                    vx = vel;
+                    vy = 0;
+                    break;
+                }
+            case 40: // baixo
+                if (vy != -vel) {
+                    vx = 0;
+                    vy = vel;
+                    break;
+                }
+            default:
+                break;
+        }
+    }
+
+    var el = document.getElementById('tela');
+    swipedetect(el, function (swipedir) {
+        switch (swipedir) {
+            case 'left':
+                if (vx != vel) {
+                    vx = -vel;
+                    vy = 0;
+                    break;
+                }
+
+            case 'top':
+                if (vy != vel) {
+                    vx = 0;
+                    vy = -vel;
+                    break;
+                }
+
+            case 'right':
+                if (vx != -vel) {
+                    vx = vel;
+                    vy = 0;
+                    break;
+                }
+
+            case 'down':
+                if (vy != -vel) {
+                    vx = 0;
+                    vy = vel;
+                    break;
+                }
+
+            default:
+                break;
+        }
+    });
+}
